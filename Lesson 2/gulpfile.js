2cee489a3c31313e1bd344193e4e0faab6cc47bb
evelyn <evelyn.b9044015@gmail.com>
2019-01-11 11:58:45 +0800

    New eslint file

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,19 +1,63 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const autoprefixer = require("gulp-autoprefixer");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
+const jasmineBrowser = require('gulp-jasmine-browser');
 
-gulp.task("default", function() {
-  console.log("hello world!");
-});
+// gulp.task('default', defaultTask);
 
-gulp.task("styles", function() {
+// function defaultTask(done) {
+//   // place code for your default task here
+//   console.log("test gulp task");
+//   done();
+// }
+gulp.task('styles', function (done) {
   gulp
-    .src("sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(
       autoprefixer({
-        browsers: ["last 2 versions"]
+        browsers: ['last 2 versions']
       })
     )
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+    done();
 });
+
+gulp.task('lint', function () {
+  return (
+    gulp
+      .src(['js/**/*.js'])
+      // eslint() attaches the lint output to the eslint property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failOnError last.
+      .pipe(eslint.failOnError())
+  );
+});
+
+gulp.task('tests', function () {
+  return gulp
+    .src('tests/spec/extraSpec.js')
+    .pipe(jasmineBrowser.specRunner({ console: true }))
+    .pipe(jasmineBrowser.headless({ driver: 'chrome' }));
+});
+
+
+
+gulp.task('default', gulp.series('styles', 'lint', function (done) {
+  gulp.watch('sass/**/*.scss', gulp.parallel('styles'));
+  gulp.watch('js/**/*.js', gulp.parallel('lint'));
+  // gulp.watch('js/**/*.js', ['lint']);
+
+  browserSync.init({
+    server: './'
+  });
+  done();
+}));
